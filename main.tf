5b56709a6b743ccfd5c56c26693e57d7f402325f
Lavanya <lavanyaeswaravaka0804@gmail.com>
2025-11-13 22:15:10 +0530

    terraform roboshop component module

--- main.tf
+++ main.tf
@@ -126,7 +126,7 @@ resource "aws_autoscaling_group" "main" {
       version = aws_launch_template.main.latest_version
     }
 
-    vpc_zone_identifier = [local.private_subnet_ids]
+    vpc_zone_identifier = local.private_subnet_ids
     target_group_arns = [aws_lb_target_group.main.arn]
 
     instance_refresh {
